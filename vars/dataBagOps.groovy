9e76a1f2bf43fe3d95af0a06d9f00a3989a8aa1f
Daniel Bright <danielcbright@gmail.com>
2020-09-29 11:12:22 -0400

    updating

--- vars/dataBagOps.groovy
+++ vars/dataBagOps.groovy
@@ -31,7 +31,7 @@ def call() {
                     knife data bag show $dirname $filename > output 2>&1 || :
                     if grep -q "The object you are looking for could not be found" output; then
                       ver_on_disk=`jq -r '.version' $f`
-                      if [ -z "$ver_on_disk" ]; then
+                      if [ "$ver_on_disk" == "null" ]; then
                         echo "$dirname:$f:$ver_on_disk:not_on_server:not_created_needs_version" >> output.txt
                       else
                         knife data bag from file $dirname $f
