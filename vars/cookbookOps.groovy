e97ec9e937a8f81413a82f92d8a98419f2e332bf
Daniel Bright <danielcbright@gmail.com>
2020-09-29 17:36:51 -0400

    updating

--- vars/cookbookOps.groovy
+++ vars/cookbookOps.groovy
@@ -7,9 +7,6 @@ def call() {
     agent any
     environment {
       PATH = "/opt/rh/rh-ruby22/root/usr/bin:$PATH"
-      LD_LIBRARY_PATH = '/opt/rh/rh-ruby22/root/usr/lib64'
-      PKG_CONFIG_PATH = '/opt/rh/rh-ruby22/root/usr/lib64/pkgconfig'
-      MANPATH = '/opt/rh/rh-ruby22/root/usr/share/man:'
     }
     stages {
       stage('Cookbook Version Validation') {
